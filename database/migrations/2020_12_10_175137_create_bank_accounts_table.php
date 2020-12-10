951065195b28dbd620ac909489076eca59bf362a
matas <matas.salkauskis@gmail.com>
2020-12-10 20:57:49 +0200

    Enum

--- database/migrations/2020_12_10_175137_create_bank_accounts_table.php
+++ database/migrations/2020_12_10_175137_create_bank_accounts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBankAccountTable extends Migration
+class CreateBankAccountsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,11 +14,12 @@ class CreateBankAccountTable extends Migration
     public function up()
     {
         Schema::create('bank_account', function (Blueprint $table) {
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->id();
             $table->integer('amount');
             $table->string('currency');
             $table->enum('membership_type', config('membership_type'));
             $table->timestamps();
+            $table->foreignId('user_id')->references('id')->on('users');
         });
     }
 
