06cdc8299016410b9d7ca10a1f4d5f19ff89ace0
matas <matas.salkauskis@gmail.com>
2020-12-10 20:06:32 +0200

    saasdsad

---
+++ database/migrations/2020_12_10_175137_create_bank_account_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBankAccountTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_account', function (Blueprint $table) {
+            $table->id();
+            $table->string('user');
+            $table->integer('amount');
+            $table->string('currency');
+            $table->string('membership-status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_account');
+    }
+}
