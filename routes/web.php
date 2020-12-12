f2f897fc6920cd6456d0ffb4ca5ddb3c9385669d
matas <matas.salkauskis@gmail.com>
2020-12-12 16:49:01 +0200

    Controller and Model

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,3 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
